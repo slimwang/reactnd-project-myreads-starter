d7182652b127789f707bb80011aace247a641dde
Slim Wang <netbot@vip.qq.com>
2018-04-28 20:41:18 +0800

    seperate bookshelf

---
+++ src/ListBooks.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Bookshelf from './Bookshelf'
+
+class ListBooks extends Component {
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <Bookshelf
+              shelf="currentlyReading"
+              books={ this.props.books.filter((book) => book.shelf==="currentlyReading") }
+            />
+            <Bookshelf
+              shelf="wantToRead"
+              books={ this.props.books.filter((book) => book.shelf==="wantToRead") }
+            />
+            <Bookshelf
+              shelf="read"
+              books={ this.props.books.filter((book) => book.shelf==="read") }
+            />
+          </div>
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        </div>
+      </div>
+     )
+  }
+}
+
+export default ListBooks;
