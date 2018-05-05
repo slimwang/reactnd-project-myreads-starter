8556c094b1c31ee27bf251fb78a03826fea672a0
Slim Wang <netbot@vip.qq.com>
2018-05-05 15:59:45 +0800

    add router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBooks from './ListBooks'
@@ -6,13 +7,6 @@ import SearchBooks from './SearchBooks'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true,
     books: [],
   }
 
@@ -35,17 +29,18 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks
-            onMoveBook={ this.handleMoveBook }
-            books={ this.state.books }
-          />
-        ) : (
+        <Route path="/" exact render={() => (
           <ListBooks
             onMoveBook={ this.handleMoveBook }
             books={ this.state.books }
           />
-        )}
+        )}/>
+        <Route path="/search" render={() => (
+          <SearchBooks
+            onMoveBook={ this.handleMoveBook }
+            books={ this.state.books }
+          />
+        )}/>
       </div>
     )
   }
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import * as BooksAPI from './BooksAPI'
 import Bookshelf from './Bookshelf'
@@ -36,7 +37,7 @@ class SearchBooks extends Component {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
             <input
               type="text"
