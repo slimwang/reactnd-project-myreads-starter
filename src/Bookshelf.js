6eefe689a7926d23336b410e199a9652aeef074a
Slim Wang <netbot@vip.qq.com>
2018-05-06 21:48:49 +0800

    None

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -13,8 +13,8 @@ class Bookshelf extends Component{
                   <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                     <div className="book-shelf-changer">
-                      <select value={this.props.shelf} onChange={(event) => this.props.onMoveBook(book, event.target.value)}>
-                        <option value="none" disabled>Move to...</option>
+                      <select value={this.props.shelf || "none"} onChange={(event) => this.props.onMoveBook(book, event.target.value)}>
+                        <option disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
                         <option value="read">Read</option>
