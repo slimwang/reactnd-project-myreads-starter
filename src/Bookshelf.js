ba372a1328cff36020212f2ea0359c149a99a42f
Slim Wang <netbot@vip.qq.com>
2018-05-07 11:07:19 +0800

    blank background image

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -11,7 +11,12 @@ class Bookshelf extends Component{
               <li key={ book.id }>
                 <div className="book">
                   <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                    <div className="book-cover" style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : ''})` 
+                      }}>
+                    </div>
                     <div className="book-shelf-changer">
                       <select value={book.shelf || "none"} onChange={(event) => this.props.onMoveBook(book, event.target.value)}>
                         <option disabled>Move to...</option>
