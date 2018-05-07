6408729ea9b20212d6e80d9c588dedee0b4f73c4
Slim Wang <netbot@vip.qq.com>
2018-05-07 13:55:36 +0800

    refactor ListBooks to stateless functional component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,39 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 import Bookshelf from './Bookshelf'
 
-class ListBooks extends Component {
-  render() {
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <Bookshelf
-              onMoveBook={ this.props.onMoveBook }
-              shelf="currentlyReading"
-              books={ this.props.books.filter((book) => book.shelf==="currentlyReading") }
-            />
-            <Bookshelf
-              onMoveBook={ this.props.onMoveBook }
-              shelf="wantToRead"
-              books={ this.props.books.filter((book) => book.shelf==="wantToRead") }
-            />
-            <Bookshelf
-              onMoveBook={ this.props.onMoveBook }
-              shelf="read"
-              books={ this.props.books.filter((book) => book.shelf==="read") }
-            />
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
-        </div>
+const ListBooks = (props) => (
+  <div className="list-books">
+    <div className="list-books-title">
+      <h1>MyReads</h1>
+    </div>
+    <div className="list-books-content">
+      <div>
+        <Bookshelf
+          onMoveBook={ props.onMoveBook }
+          shelf="currentlyReading"
+          books={ props.books.filter((book) => book.shelf==="currentlyReading") }
+        />
+        <Bookshelf
+          onMoveBook={ props.onMoveBook }
+          shelf="wantToRead"
+          books={ props.books.filter((book) => book.shelf==="wantToRead") }
+        />
+        <Bookshelf
+          onMoveBook={ props.onMoveBook }
+          shelf="read"
+          books={ props.books.filter((book) => book.shelf==="read") }
+        />
       </div>
-     )
-  }
-}
+    </div>
+    <div className="open-search">
+      <Link to="/search">Add a book</Link>
+    </div>
+  </div>
+)
 
 export default ListBooks;
